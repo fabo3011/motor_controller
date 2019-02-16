6037267f5bc436c134cd3dffb850175985f0502d
Ivan Ochoa <a01209351@itesm.mx>
2019-02-15 20:09:44 -0600

    saving my work

--- roboclaw/scripts/tankMode.py
+++ roboclaw/scripts/tankMode.py
@@ -7,54 +7,44 @@ from roboclaw import Roboclaw
 class tank:
     def __init__(self,port,baud,pwmLimit,motorsID):
         self.pord = port
-        print("koko")
         self.brte = baud
         self.pwml = pwmLimit
         self.msID = motorsID
-        print("skdf")
         self.rccm = self.createRC(port,baud)
-        print("caca")
         self.open()
 
     def tankDrive(self,joy):
-        print("dfdf")
         if joy.y > 0.2:
             self.goForward('rigth',self.fixPwm(joy.y))
-        elif joy.y < 0.2:
+        elif joy.y < -0.2:
             self.goBackward('rigth',self.fixPwm(-joy.y))
         else:
             self.setTo0('rigth')
 
         if joy.x > 0.2:
             self.goForward('left',self.fixPwm(joy.x))
-        elif joy.x < 0.2:
+        elif joy.x < -0.2:
             self.goBackward('left',self.fixPwm(-joy.x))
         else:
             self.setTo0('left')
     def armDrive(self,arm):
-        print("dfsd")
         self.pich(arm.y)
-        print("sdfsdfsdfs")
         self.yaw(arm.z)
-        print("ffif")
         self.Forward(arm.x)
-        print("fsfdsfdsdf")
 
-        
+
     def open(self):
-        print("caca2")
         for i in range(0,3):
             #print (self.rccm)
             if self.rccm[i].Open():
                print(self.rccm[i]._port)
             else: 
-                print("Error: cannot open port: " + self.pord[i])
-        
+                exit("Error: cannot open port: " + self.pord[i])
+
     def createRC(self,port,baud):
         print(port)
         listrc = [None,None,None]
         for i in range(0,3):
-            print("kokoko")
             listrc[i] = Roboclaw('/dev/' + port[i], baud)
         return listrc
 
@@ -65,15 +55,12 @@ class tank:
         elif side == 'left':
             for i in range(1,2):
                 self.rccm[i].BackwardM1(self.msID[1],pwm)
-    
+
     def Forward(self,x):
-        print("uno")
         if x == 1:
-            print('fdf')
             self.goForward('right',50)
             self.goForward('left',50)    
         else:
-            print('wewew')
             self.setTo0('right')
             self.setTo0('left')    
 
@@ -86,7 +73,7 @@ class tank:
                 self.rccm[i].ForwardM1(self.msID[1],pwm)
         else:
             print("Warnig: command not found.")
-    
+
     def pich(self,id):
         if id == 1:
             self.rccm[0].ForwardM1(self.msID[0],80)
@@ -94,7 +81,7 @@ class tank:
             self.rccm[0].BackwardM1(self.msID[0],80)
         else:
             self.rccm[0].ForwardM1(self.msID[0],0)
-    
+
     def yaw(self,id):
         if id == 1:
             self.rccm[0].ForwardM1(self.msID[1],20)
@@ -110,6 +97,6 @@ class tank:
         if side == 'left':
             for i in range(1,2):
                 self.rccm[i].ForwardM1(self.msID[1],0)
-    
+
     def fixPwm(self,percentage):
         return long(round(percentage*self.pwml,2))
