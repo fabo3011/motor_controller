63d33d1b077223b393d7b508db556887736500a8
scanf30 <a01209351@itesm.mx>
2019-02-17 13:45:54 -0600

    Test 1 2019

--- roboclaw/scripts/tankMode.py
+++ roboclaw/scripts/tankMode.py
@@ -7,17 +7,13 @@ from roboclaw import Roboclaw
 class tank:
     def __init__(self,port,baud,pwmLimit,motorsID):
         self.pord = port
-        print("koko")
         self.brte = baud
         self.pwml = pwmLimit
         self.msID = motorsID
-        print("skdf")
         self.rccm = self.createRC(port,baud)
-        print("caca")
         self.open()
 
     def tankDrive(self,joy):
-        print("dfdf")
         if joy.y > 0.2:
             self.goForward('rigth',self.fixPwm(joy.y))
         elif joy.y < 0.2:
@@ -31,18 +27,8 @@ class tank:
             self.goBackward('left',self.fixPwm(-joy.x))
         else:
             self.setTo0('left')
-    def armDrive(self,arm):
-        print("dfsd")
-        self.pich(arm.y)
-        print("sdfsdfsdfs")
-        self.yaw(arm.z)
-        print("ffif")
-        self.Forward(arm.x)
-        print("fsfdsfdsdf")
-
         
     def open(self):
-        print("caca2")
         for i in range(0,3):
             #print (self.rccm)
             if self.rccm[i].Open():
@@ -54,7 +40,6 @@ class tank:
         print(port)
         listrc = [None,None,None]
         for i in range(0,3):
-            print("kokoko")
             listrc[i] = Roboclaw('/dev/' + port[i], baud)
         return listrc
 
@@ -67,48 +52,29 @@ class tank:
                 self.rccm[i].BackwardM1(self.msID[1],pwm)
     
     def Forward(self,x):
-        print("uno")
         if x == 1:
-            print('fdf')
             self.goForward('right',50)
             self.goForward('left',50)    
         else:
-            print('wewew')
             self.setTo0('right')
             self.setTo0('left')    
 
     def goBackward(self,side,pwm):
         if side == 'rigth':
-            for i in range(1,2):
+            for i in range(1,3):
                 self.rccm[i].BackwardM1(self.msID[0],pwm)
         elif side == 'left':
-            for i in range(1,2):
+            for i in range(1,3):
                 self.rccm[i].ForwardM1(self.msID[1],pwm)
         else:
             print("Warnig: command not found.")
     
-    def pich(self,id):
-        if id == 1:
-            self.rccm[0].ForwardM1(self.msID[0],80)
-        elif id == -1:
-            self.rccm[0].BackwardM1(self.msID[0],80)
-        else:
-            self.rccm[0].ForwardM1(self.msID[0],0)
-    
-    def yaw(self,id):
-        if id == 1:
-            self.rccm[0].ForwardM1(self.msID[1],20)
-        elif id == -1:
-            self.rccm[0].BackwardM1(self.msID[1],20)
-        else:
-            self.rccm[0].ForwardM1(self.msID[1],0)
-
     def setTo0(self,side):
         if side == 'rigth':
-            for i in range(1,2):
+            for i in range(1,3):
                 self.rccm[i].ForwardM1(self.msID[0],0)
         if side == 'left':
-            for i in range(1,2):
+            for i in range(1,3):
                 self.rccm[i].ForwardM1(self.msID[1],0)
     
     def fixPwm(self,percentage):
--- roboclaw/scripts/tank_node.py
+++ roboclaw/scripts/tank_node.py
@@ -7,43 +7,24 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Vector3
 from tankMode import tank 
 
-print ("lolo")
-
 class tank_node:
     def __init__(self):
-        print("kokok")
         self.mctl = Vector3()
         self.actl = Vector3()
-        print (self.mctl)
         self.rover = tank(port=['ttyACM0','ttyACM1','ttyACM2'],baud='115200',pwmLimit=102,motorsID=[128,129])
-        print("popo")
         rospy.Subscriber('joy',Joy,self.sideCB)
-        rospy.Subscriber('posiTomate',Vector3,self.armCB)
 
         r = rospy.Rate(10)
-        print("fasdkf")
         while not rospy.is_shutdown():
             print(self.actl)
-            if self.mctl == 1:
-                print("fsd")
-                self.rover.tankDrive(self.mctl)
-            else:
-                print("sfd")
-                self.rover.armDrive(self.actl)
+            self.rover.tankDrive(self.mctl)
             r.sleep()
     
     def sideCB(self,joy):
         self.mctl.x = joy.axes[1]
         self.mctl.y = joy.axes[4]
-        self.mctl.z = joy.button[1]
-    
-    def armCB(self,cam):
-        self.actl.x = 1
-        self.actl.z = cam.y
-        self.actl.y = cam.z
 
 if __name__=="__main__":
-    print("hola")
     rospy.init_node('rover')
     try:
 
