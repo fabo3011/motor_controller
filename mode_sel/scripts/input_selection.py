f9e1295925689118b5d2cfd18dfc3599f348bd2a
Iván Ochoa <ivan.aochoa.g@gmail.com>
2019-04-17 15:14:49 -0500

    redy for test. hoe.

---
+++ mode_sel/scripts/input_selection.py
@@ -0,0 +1,62 @@
+#!usr/bin/env python
+
+import roslib
+import rospy
+
+from sensor_msgs.msg import Joy
+from std_msgs.msg import Float32
+from std_msgs.msg import Char
+
+class input_sel:
+    def __init__ (self):
+        self.joy2rVel = Float32
+        self.joy2lVel = Float32
+        self.aut2rVel = Float32
+        self.aut2lVel = Float32
+        self.left_Vel = Float32
+        self.rightVel = Float32
+
+        rospy.Subscriber('/Motors/mode',Char,self.sideCB)
+        rospy.Subscriber('joy',Joy,self.joy2linvel)
+        rospy.Subscriber('/BaseController/left_vel',Float32,self.leftCB)
+        rospy.Subscriber('/BaseController/right_vel',Float32,self.rightCB)
+
+        lvel_pub = rospy.Publisher('/Motors/left_vel',Float32,queue_size=10)
+        rvel_pub = rospy.Publisher('/Motors/right_vel',Float32,queue_size=10)
+
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+            rospy.loginfo("left vel: %f   right vel: %f" % self.left_Vel %self.rightVel )
+            lvel_pub.publish(self.left_Vel)
+            rvel_pub.publish(self.rightVel)
+            rate.sleep()
+
+    def sideCB (self,cmode):
+        if cmode == 'j':
+            self.left_Vel = self.joy2lVel
+            self.rightVel = self.joy2rVel
+        elif cmode == 'i':
+            self.left_Vel = self.aut2lVel
+            self.rightVel = self.aut2rVel
+        elif cmode == 's':
+            self.left_Vel = 0
+            self.rightVel = 0
+        else:
+            rospy.loginfo('MAAAMEEEEES, ponte vergas y pon < j > para manual o < i > para autonomia o interfaz')
+            self.left_Vel = 0
+            self.rightVel = 0
+
+    def joy2linvel(self,joy):
+        self.joy2rvel = joy[1]
+        self.joy2lVel = joy[4]
+    
+    def leftCB (self,float32):
+        self.aut2lVel = float32
+
+    def rightCB (self,float32):
+        self.aut2rVel = float32
+
+if __name__ == '__main__':
+    rospy.init_node('Mode_selection',anonymous=True)
+    input_sel()
--- roboclaw/scripts/tank_node.py
+++ roboclaw/scripts/tank_node.py
@@ -3,15 +3,16 @@ import roslib
 import rospy
 import math
 
-from sensor_msgs.msg import Joy
+from std_msgs.msg import Float32
 from geometry_msgs.msg import Vector3
 from tankMode import tank 
 
 class tank_node:
     def __init__(self):
-        self.mctl = Vector3()
+        self.mctl = Vector3
         self.rover = tank(port='ttyACM0',baud='115200',pwmLimit=102,motorsID=[128,129,130,131,132,133])
-        rospy.Subscriber('joy',Joy,self.sideCB)
+        rospy.Subscriber('/Motors/',Float32,self.left)
+        rospy.Subscriber('/Motors/')
 
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
@@ -19,10 +20,11 @@ class tank_node:
             self.rover.tankDrive(self.mctl)
             r.sleep()
 
-    def sideCB(self,joy):
-        self.mctl.x = joy.axes[1]
-        self.mctl.y = joy.axes[4]
-        print(self.mctl)
+    def left(self,vel):
+        self.mctl.x = vel
+
+    def right(self,vel):
+        self.mctl.y = vel
 
 if __name__=="__main__":
     rospy.init_node('MotorsAlv')
