fc3f77e33c3a80e71f5ddddb2e215e043362d6f7
Iván Ochoa <ivan.aochoa.g@gmail.com>
2019-05-16 20:02:02 -0500

    wuuujuuu

--- roboclaw/scripts/tank_node.py
+++ roboclaw/scripts/tank_node.py
@@ -4,20 +4,27 @@ import rospy
 import math
 
 from std_msgs.msg import Float32
+from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import Vector3
 from tankMode import tank 
 
 class tank_node:
     def __init__(self):
-        self.mctl = Vector3
+        self.mctl = Vector3()
+        self.speed = Float32MultiArray()
         self.rover = tank(port='ttyACM0',baud='115200',pwmLimit=102,motorsID=[128,129,130,131,132,133])
+        
         rospy.Subscriber('/Motors/left_vel',Float32,self.left)
         rospy.Subscriber('/Motors/right_vel',Float32,self.right)
 
+        speed_pub = rospy.Publisher('/Motors/motors_speed',Float32MultiArray,queue_size=10)
+        
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
+            rospy.loginfo(self.speed)
             #print(self.mctl)
-            self.rover.tankDrive(self.mctl)
+            self.speed.data = self.rover.tankDrive(self.mctl)
+            speed_pub.publish(self.speed)
             r.sleep()
 
     def left(self,vel):
