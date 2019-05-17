54ce5f9a9626dad0a5ba4d4212f2a8a1391eb0d4
Iván Ochoa <ivan.aochoa.g@gmail.com>
2019-05-17 11:02:10 -0500

    output error try fix

--- mode_sel/scripts/input_selection.py
+++ mode_sel/scripts/input_selection.py
@@ -27,7 +27,7 @@ class input_sel:
         rate = rospy.Rate(10)
 
         while not rospy.is_shutdown():
-            rospy.loginfo("left vel: %f   right vel: %f" % self.left_Vel %self.rightVel )
+            rospy.loginfo("left vel: %f   right vel: %f" %float(self.left_Vel) %float(self.rightVel) )
             lvel_pub.publish(self.left_Vel)
             rvel_pub.publish(self.rightVel)
             rate.sleep()
